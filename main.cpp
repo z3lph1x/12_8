ace633250699342e813f707e2294191c943982d7
z3lph1x <72938782+z3lph1x@users.noreply.github.com>
2021-10-20 21:28:39 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+int main() {
+    double x1;
+    std::cin >> x1;
+    double e = 1;
+    if (x1 < 1 / sqrt(2)) {
+        double x2 = 1 / (2 * sqrt(2)) * std::exp(x1 * x1 - 0.5);
+        if (std::abs(x2 - x1) < 0.001) {
+            std::cout << x2;
+            return 0;
+        }
+        while (e >= 0.001) {
+            x1 = x2;
+            x2 = 1 / (2 * sqrt(2)) * std::exp(x1 * x1 - 0.5);
+            std::cout << x2;
+            e = std::abs(x2 - x1);
+        }
+        std::cout << std::endl;
+        std::cout << std::setprecision(3) << x2;
+        return 0;
+    } else if (x1 > 1 / sqrt(2)) {
+        double x2 = sqrt(log(2 * sqrt(2) * x1) + 0.5);
+        if (std::abs(x2 - x1) < 0.001) {
+            std::cout << x2;
+            return 0;
+        }
+        while (e >= 0.001) {
+            x1 = x2;
+            x2 = sqrt(log(2 * sqrt(2) * x1) + 0.5);
+            std::cout << x2;
+            e = std::abs(x2 - x1);
+        }
+        std::cout << std::endl;
+        std::cout << std::setprecision(4) << x2;
+        return 0;
+    }
+}
